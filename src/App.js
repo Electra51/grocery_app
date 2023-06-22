4ead0db2a53a4fe337b6d97af21c1d3504c98361
safayet <safayet@merchantbay.com>
2023-06-22 13:14:33 +0600

    initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from "antd";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <PageContent />
+      <AppFooter />
     </div>
   );
+}
+
+function AppHeader() {
+  return <div className="appHeader">AppHeader</div>;
+}
+function AppFooter() {
+  return <div className="appFooter">AppFooter</div>;
+}
+function PageContent() {
+  return <div className="pageContent">PageContent</div>;
 }
 
 export default App;
